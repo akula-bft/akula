d71e953930c47453430113fe4a7f3434a2ce00e4
Artem Vorotnikov <artem@vorotnikov.me>
2021-02-02 09:33:06 +0300

    WIP

--- src/kv_mdbx.rs
+++ src/kv_mdbx.rs
@@ -1,6 +1,7 @@
-use crate::traits::Cursor;
+use crate::traits::{self, Cursor};
 use anyhow::anyhow;
 use bytes::Bytes;
+use futures::channel::mpsc::UnboundedReceiver;
 use heed::{Database, Env, EnvOpenOptions, RoIter, RwIter, RwTxn};
 use serde::__private::de;
 use std::{
@@ -12,68 +13,121 @@ use std::{
 use tokio::{
     select,
     sync::{
-        mpsc::{channel, Sender},
+        mpsc::{channel, unbounded_channel, Sender, UnboundedSender},
         oneshot::{channel as oneshot, Sender as OneshotSender},
     },
     task::LocalSet,
 };
 
-pub struct WriteCursor<'env> {
-    inner: Arc<DbServerInner>,
-    _marker: PhantomData<&'env ()>,
+pub struct WriteCursor<'txn> {
+    cmd_tx: Sender<TxCommand>,
+    cursor_id: u64,
+    cleanup_tx: UnboundedSender<TxCleanupCommand>,
+    _marker: PhantomData<&'txn ()>,
+}
+
+impl Drop for WriteCursor<'_> {
+    fn drop(&mut self) {
+        let _ = self
+            .cleanup_tx
+            .send(TxCleanupCommand::Cursor { id: self.cursor_id });
+    }
+}
+
+enum TxCleanupCommand {
+    Cursor { id: u64 },
+}
+
+enum TxCommand {
+    Cursor {
+        bucket_name: String,
+        cb: OneshotSender<anyhow::Result<(u64, UnboundedSender<TxCleanupCommand>)>>,
+    },
 }
 
 pub struct WriteTxn<'env> {
-    inner: Arc<DbServerInner>,
+    cmd_tx: Sender<TxCommand>,
+    cleanup_tx: UnboundedSender<TxCleanupCommand>,
+    cancellation: OneshotSender<()>,
     _marker: PhantomData<&'env ()>,
 }
 
-impl<'env> WriteTxn<'env> {
-    pub async fn cursor(&self, bucket_name: String) -> anyhow::Result<WriteCursor<'_>> {}
-    fn make_cursor<'env: 'txn>(
-        env: &'env Env,
-        txn: &'txn mut RwTxn<'env, 'env>,
+impl<'env, 'txn> WriteTxn<'env> {
+    fn make_cursor(
+        env: &Env,
+        txn: &'txn RwTxn<'env, 'env>,
+        cursors: &mut HashMap<u64, RwIter<'txn, Bytes, Bytes>>,
         bucket_name: &str,
-    ) -> anyhow::Result<WriteCursor<'txn>> {
+    ) -> anyhow::Result<u64> {
         let db = env
             .open_database(Some(bucket_name))?
             .ok_or_else(|| anyhow!("no database!"))?;
 
-        Ok(WriteCursor {
-            inner: db.iter_mut(txn)?,
-        })
-    }
-
-    fn run_command<'env: 'txn>(
-        env: &'env Env,
-        txn: &'txn mut RwTxn<'env, 'env>,
-        cmd: TxCommand<'txn>,
-    ) {
-        match cmd {
-            TxCommand::Cursor { bucket_name, cb } => {
-                cb.send(Self::make_cursor(env, txn, bucket_name.as_str()));
+        loop {
+            let id = rand::random();
+            if let Entry::Vacant(entry) = cursors.entry(id) {
+                entry.insert(db.iter_mut(&txn)?);
+                break Ok(id);
             }
         }
     }
-}
 
-enum DbCommand {
-    WriteTxn {
-        cb: OneshotSender<anyhow::Result<u64>>,
-    },
-    Cursor {
-        bucket_name: String,
-        cb: OneshotSender<anyhow::Result<u64>>,
+    fn run_command(
+        env: &Env,
+        txn: &'txn RwTxn<'env, 'env>,
+        cursors: &mut HashMap<u64, RwIter<'txn, Bytes, Bytes>>,
+        cleanup_tx: &UnboundedSender<TxCleanupCommand>,
+        cmd: TxCommand,
+    ) {
+        match cmd {
+            TxCommand::Cursor { bucket_name, cb } => {
+                let _ = cb.send(
+                    Self::make_cursor(env, txn, cursors, &bucket_name)
+                        .map(|id| (id, cleanup_tx.clone())),
+                );
+            }
+        }
+    }
+
+    fn run_cleanup(
+        _env: &Env,
+        _txn: &'txn RwTxn<'env, 'env>,
+        cursors: &mut HashMap<u64, RwIter<'txn, Bytes, Bytes>>,
+        cmd: TxCleanupCommand,
+    ) {
+        match cmd {
+            TxCleanupCommand::Cursor { id } => {
+                cursors.remove(&id).unwrap();
+            }
+        }
+    }
+
+    pub async fn cursor<B: ToString>(&self, bucket_name: B) -> anyhow::Result<WriteCursor<'_>> {
+        let (cb, rx) = oneshot();
+        assert!(self
+            .cmd_tx
+            .send(TxCommand::Cursor {
+                bucket_name: bucket_name.to_string(),
+                cb,
+            })
+            .await
+            .is_ok());
+
+        let (cursor_id, cleanup_tx) = rx.await.unwrap()?;
+
+        Ok(WriteCursor {
+            cmd_tx: self.cmd_tx.clone(),
+            cursor_id,
+            cleanup_tx,
+            _marker: PhantomData,
+        })
     }
 }
 
-struct DbServerInner {
+pub struct DbServer {
+    env: Arc<Env>,
     local_set: Arc<LocalSet>,
-    cmd_tx: Sender<DbCommand>,
-    cancellation: OneshotSender<()>,
 }
-
-pub struct DbServer(Arc<DbServerInner>);
 
 impl DbServer {
     pub fn open(
@@ -82,65 +136,63 @@ impl DbServer {
         options: EnvOpenOptions,
     ) -> anyhow::Result<Self> {
         let env = Arc::new(options.open(path)?);
+        Ok(Self { env, local_set })
+    }
+
+    /// Create a new write transaction
+    pub async fn write_txn(&self) -> anyhow::Result<WriteTxn<'_>> {
         let (cmd_tx, mut cmd_rx) = channel(1);
+        // Unbounded channel for cleanup as no AsyncDrop in Rust.
+        let (cleanup_tx, mut cleanup_rx) = unbounded_channel();
+        let (tx, rx) = oneshot();
         let (cancellation, mut dropped) = oneshot();
 
-        local_set.spawn_local(async move {
-            let txns = Default::default();
-            loop {
-                select! {
-                    _ = &mut dropped => {
-                        return;
-                    }
-                    cmd = cmd_rx.recv() => {
-                        if let Some(cmd) = cmd {
-                            DbServer::run_command(&env, &mut txns, cmd);
-                        } else {
-                            return;
+        let env = self.env.clone();
+
+        self.local_set.spawn_local({
+            let cleanup_tx = cleanup_tx.clone();
+            async move {
+            match env.write_txn() {
+                Err(e) => {
+                    tx.send(Err(e)).unwrap();
+                }
+                Ok(txn) => {
+                    tx.send(Ok(())).unwrap();
+
+                    let mut cursors = HashMap::default();
+
+                    loop {
+                        select! {
+                            _ = &mut dropped => {
+                                return;
+                            }
+                            cmd = cmd_rx.recv() => {
+                                if let Some(cmd) = cmd {
+                                    WriteTxn::run_command(&env, &txn, &mut cursors, &cleanup_tx, cmd);
+                                } else {
+                                    return;
+                                }
+                            }
+                            cleanup = cleanup_rx.recv() => {
+                                if let Some(cmd) = cleanup {
+                                    WriteTxn::run_cleanup(&env, &txn, &mut cursors, cmd);
+                                } else {
+                                    return;
+                                }
+                            }
                         }
                     }
                 }
             }
-        });
+        }});
 
-        Ok(Self(Arc::new(DbServerInner {
-            local_set,
+        rx.await.unwrap()?;
+
+        Ok(WriteTxn {
             cmd_tx,
+            cleanup_tx,
             cancellation,
-        })))
-    }
-
-    fn run_command<'env>(
-        env: &'env Env,
-        txns: &mut HashMap<u64, RwTxn<'env, 'env>>,
-        cmd: DbCommand,
-    ) {
-        match cmd {
-            DbCommand::WriteTxn { cb } => {
-                cb.send(
-                    env.write_txn()
-                        .map(|txn| loop {
-                            let key = rand::random();
-                            if let Entry::Vacant(entry) = txns.entry(key) {
-                                entry.insert(txn);
-                                break key;
-                            }
-                        })
-                        .map_err(anyhow::Error::from),
-                );
-            }
-            DbCommand::Cursor { cb } => {
-                cb.send(env
-                    .open_database(Some(bucket_name))?
-                    .ok_or_else(|| anyhow!("no database!")).map(|db| db.iter_mut(txn)));
-            }
-        }
-    }
-
-    pub async fn write_transaction(&self) -> WriteTxn<'_> {
-        WriteTxn {
-            inner: self.inner.clone(),
             _marker: PhantomData,
-        }
+        })
     }
 }
