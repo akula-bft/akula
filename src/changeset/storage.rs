5ecfba808da69cd15ea9ab52057da29e5efae000
Artem Vorotnikov <artem@vorotnikov.me>
2021-05-02 19:00:20 +0300

    changeset: multiple_incarnations_of_the_same_contract test

--- src/changeset/storage.rs
+++ src/changeset/storage.rs
@@ -36,7 +36,12 @@ impl tables::PlainStorageChangeSet {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::dbutils;
+    use crate::{
+        dbutils,
+        kv::traits::{MutableCursor, MutableKV, MutableTransaction},
+    };
+    use ethereum_types::Address;
+    use hex_literal::hex;
 
     const NUM_OF_CHANGES: &[usize] = &[1, 3, 10, 100];
 
@@ -167,5 +172,153 @@ mod tests {
     fn run_test<F: Fn(usize, usize)>(f: F, elements: usize, keys: usize) {
         println!("elements: {}, keys: {}", elements, keys);
         (f)(elements, keys);
+    }
+
+    #[tokio::test]
+    async fn multiple_incarnations_of_the_same_contract() {
+        let env = crate::kv::new_mem_database().unwrap();
+        let tx = env.begin_mutable().await.unwrap();
+
+        type CsTable = tables::PlainStorageChangeSet;
+
+        let mut cs = tx.mutable_cursor_dupsort::<CsTable>().await.unwrap();
+
+        let contract_a = Address::from(hex!("6f0e0cdac6c716a00bd8db4d0eee4f2bfccf8e6a"));
+        let contract_b = Address::from(hex!("c5acb79c258108f288288bc26f7820d06f45f08c"));
+        let contract_c = Address::from(hex!("1cbdd8336800dc3fe27daf5fb5188f0502ac1fc7"));
+        let contract_d = Address::from(hex!("d88eba4c93123372a9f67215f80477bc3644e6ab"));
+
+        let key1 = common::Hash::from(hex!(
+            "a4e69cebbf4f8f3a1c6e493a6983d8a5879d22057a7c73b00e105d7c7e21efbc"
+        ));
+        let key2 = common::Hash::from(hex!(
+            "0bece5a88f7b038f806dbef77c0b462506e4b566c5be7dd44e8e2fc7b1f6a99c"
+        ));
+        let key3 = common::Hash::from(hex!(
+            "0000000000000000000000000000000000000000000000000000000000000001"
+        ));
+        let key4 = common::Hash::from(hex!(
+            "4fdf6c1878d2469b49684effe69db8689d88a4f1695055538501ff197bc9e30e"
+        ));
+        let key5 = common::Hash::from(hex!(
+            "aa2703c3ae5d0024b2c3ab77e5200bb2a8eb39a140fad01e89a495d73760297c"
+        ));
+        let key6 = common::Hash::from(hex!(
+            "000000000000000000000000000000000000000000000000000000000000df77"
+        ));
+        let key7 = common::Hash::from(hex!(
+            "0000000000000000000000000000000000000000000000000000000000000000"
+        ));
+
+        let val1 = hex!("33bf0d0c348a2ef1b3a12b6a535e1e25a56d3624e45603e469626d80fd78c762");
+        let val2 = hex!("0000000000000000000000000000000000000000000000000000000000000459");
+        let val3 = hex!("0000000000000000000000000000002506e4b566c5be7dd44e8e2fc7b1f6a99c");
+        let val4 = hex!("207a386cdf40716455365db189633e822d3a7598558901f2255e64cb5e424714");
+        let val5 = hex!("0000000000000000000000000000000000000000000000000000000000000000");
+        let val6 = hex!("ec89478783348038046b42cc126a3c4e351977b5f4cf5e3c4f4d8385adbf8046");
+
+        let ch = vec![
+            (
+                dbutils::plain_generate_composite_storage_key(contract_a, 2, key1),
+                Bytes::from(&val1),
+            ),
+            (
+                dbutils::plain_generate_composite_storage_key(contract_a, 1, key5),
+                Bytes::from(&val5),
+            ),
+            (
+                dbutils::plain_generate_composite_storage_key(contract_a, 2, key6),
+                Bytes::from(&val6),
+            ),
+            (
+                dbutils::plain_generate_composite_storage_key(contract_b, 1, key2),
+                Bytes::from(&val2),
+            ),
+            (
+                dbutils::plain_generate_composite_storage_key(contract_b, 1, key3),
+                Bytes::from(&val3),
+            ),
+            (
+                dbutils::plain_generate_composite_storage_key(contract_c, 5, key4),
+                Bytes::from(&val4),
+            ),
+        ]
+        .into_iter()
+        .map(|(k, v)| Change::new(k, v))
+        .collect();
+
+        let mut c = tx.mutable_cursor_dupsort::<CsTable>().await.unwrap();
+        for (k, v) in CsTable::encode(1, &ch) {
+            c.put(&k, &v).await.unwrap()
+        }
+
+        assert_eq!(
+            CsTable::find_with_incarnation(
+                &mut cs,
+                1,
+                &dbutils::plain_generate_composite_storage_key(contract_a, 2, key1),
+            )
+            .await
+            .unwrap()
+            .unwrap(),
+            val1
+        );
+
+        assert_eq!(
+            CsTable::find_with_incarnation(
+                &mut cs,
+                1,
+                &dbutils::plain_generate_composite_storage_key(contract_b, 1, key3)
+            )
+            .await
+            .unwrap()
+            .unwrap(),
+            val3
+        );
+
+        assert_eq!(
+            CsTable::find_with_incarnation(
+                &mut cs,
+                1,
+                &dbutils::plain_generate_composite_storage_key(contract_a, 1, key5)
+            )
+            .await
+            .unwrap()
+            .unwrap(),
+            val5
+        );
+
+        assert_eq!(
+            CsTable::find_with_incarnation(
+                &mut cs,
+                1,
+                &dbutils::plain_generate_composite_storage_key(contract_a, 1, key1)
+            )
+            .await
+            .unwrap(),
+            None
+        );
+
+        assert_eq!(
+            CsTable::find_with_incarnation(
+                &mut cs,
+                1,
+                &dbutils::plain_generate_composite_storage_key(contract_d, 2, key1)
+            )
+            .await
+            .unwrap(),
+            None
+        );
+
+        assert_eq!(
+            CsTable::find_with_incarnation(
+                &mut cs,
+                1,
+                &dbutils::plain_generate_composite_storage_key(contract_b, 1, key7)
+            )
+            .await
+            .unwrap(),
+            None
+        );
     }
 }
