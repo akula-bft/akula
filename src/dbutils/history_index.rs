e72968339cf7700f9e2b822fa5caf4691280ded7
Artem Vorotnikov <artem@vorotnikov.me>
2021-05-31 14:36:57 +0300

    Fix index_chunk_key

--- src/dbutils/history_index.rs
+++ src/dbutils/history_index.rs
@@ -4,23 +4,21 @@ use bytes::Bytes;
 pub fn index_chunk_key(key: &[u8], block_number: u64) -> Bytes {
     match key.len() {
         // hashed state, accounts
-        common::HASH_LENGTH | common::ADDRESS_LENGTH => key
-            .iter()
+        common::HASH_LENGTH | common::ADDRESS_LENGTH => std::iter::empty()
+            .chain(key)
             .chain(&block_number.to_be_bytes())
             .copied()
             .collect(),
         // hashed state storage
-        dbutils::COMPOSITE_STORAGE_KEY_LENGTH => key
-            [..common::HASH_LENGTH + common::HASH_LENGTH + common::BLOCK_NUMBER_LENGTH]
-            .iter()
+        dbutils::COMPOSITE_STORAGE_KEY_LENGTH => std::iter::empty()
+            .chain(&key[..common::HASH_LENGTH])
             .chain(&key[common::HASH_LENGTH + common::INCARNATION_LENGTH..])
             .chain(&block_number.to_be_bytes())
             .copied()
             .collect(),
         // plain state storage
-        dbutils::PLAIN_COMPOSITE_STORAGE_KEY_LENGTH => key
-            [..common::ADDRESS_LENGTH + common::HASH_LENGTH + common::BLOCK_NUMBER_LENGTH]
-            .iter()
+        dbutils::PLAIN_COMPOSITE_STORAGE_KEY_LENGTH => std::iter::empty()
+            .chain(&key[..common::ADDRESS_LENGTH])
             .chain(&key[common::ADDRESS_LENGTH + common::INCARNATION_LENGTH..])
             .chain(&block_number.to_be_bytes())
             .copied()
