36ce11f69314f5bc63ad8d6eeefbc98374612cac
battlmonstr <battlmonstr@users.noreply.github.com>
2021-07-11 20:06:14 +0200

    Parse common incoming messages
    * BlockHeaders
    * NewBlock
    * NewPooledTransactionHashes

--- src/downloader/message_decoder.rs
+++ src/downloader/message_decoder.rs
@@ -2,12 +2,19 @@ use crate::downloader::messages::*;
 
 pub fn decode_rlp_message(id: EthMessageId, message_bytes: &[u8]) -> anyhow::Result<Message> {
     let message: Message = match id {
-        EthMessageId::GetBlockHeaders => {
-            Message::GetBlockHeaders(rlp::decode::<GetBlockHeadersMessage>(message_bytes)?)
-        }
         EthMessageId::NewBlockHashes => {
             Message::NewBlockHashes(rlp::decode::<NewBlockHashesMessage>(message_bytes)?)
         }
+        EthMessageId::GetBlockHeaders => {
+            Message::GetBlockHeaders(rlp::decode::<GetBlockHeadersMessage>(message_bytes)?)
+        }
+        EthMessageId::BlockHeaders => {
+            Message::BlockHeaders(rlp::decode::<BlockHeadersMessage>(message_bytes)?)
+        }
+        EthMessageId::NewBlock => Message::NewBlock(rlp::decode::<NewBlockMessage>(message_bytes)?),
+        EthMessageId::NewPooledTransactionHashes => Message::NewPooledTransactionHashes(
+            rlp::decode::<NewPooledTransactionHashesMessage>(message_bytes)?,
+        ),
         _ => anyhow::bail!("decode_rlp_message: unsupported message {:?}", id),
     };
     Ok(message)
@@ -16,8 +23,11 @@ pub fn decode_rlp_message(id: EthMessageId, message_bytes: &[u8]) -> anyhow::Res
 impl rlp::Encodable for Message {
     fn rlp_append(&self, stream: &mut rlp::RlpStream) {
         match self {
-            Message::GetBlockHeaders(message) => message.rlp_append(stream),
             Message::NewBlockHashes(message) => message.rlp_append(stream),
+            Message::GetBlockHeaders(message) => message.rlp_append(stream),
+            Message::BlockHeaders(message) => message.rlp_append(stream),
+            Message::NewBlock(message) => message.rlp_append(stream),
+            Message::NewPooledTransactionHashes(message) => message.rlp_append(stream),
         }
     }
 }
@@ -26,7 +36,7 @@ impl rlp::Encodable for Message {
 mod tests {
     use crate::downloader::{
         message_decoder::decode_rlp_message,
-        messages::{EthMessageId, Message},
+        messages::{BlockHashAndNumber, EthMessageId, Message, NewBlockHashesMessage},
     };
     use ethereum_types::H256;
     use hex_literal::hex;
@@ -41,17 +51,16 @@ mod tests {
         let bytes = rlp::encode(&some_message);
         assert_eq!(&*bytes, expected_bytes);
 
-        if let Message::NewBlockHashes(message) = some_message {
-            assert_eq!(message.ids.len(), 1);
-            assert_eq!(
-                message.ids[0].0,
-                H256(hex!(
-                    "7100614faba6650b53fe0913ed7267bcc968eb362e3df908645a50aa526c72ba"
-                ))
-            );
-            assert_eq!(message.ids[0].1, 10567341);
-        } else {
-            assert!(false, "unexpected message type");
-        }
+        assert_eq!(
+            some_message,
+            Message::NewBlockHashes(NewBlockHashesMessage {
+                ids: vec![BlockHashAndNumber {
+                    hash: H256(hex!(
+                        "7100614faba6650b53fe0913ed7267bcc968eb362e3df908645a50aa526c72ba"
+                    )),
+                    number: 10567341,
+                },],
+            })
+        );
     }
 }
