e8f9ad7f2574bf28a992e21974ff94f765b9c8ac
Artem Vorotnikov <artem@vorotnikov.me>
2021-08-20 19:25:48 +0300

    memory_buffer.rs -> in_memory_state.rs

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,11 +1,11 @@
 mod database;
 mod delta;
 mod history;
+mod in_memory_state;
 mod interface;
 mod intra_block_state;
-mod memory_buffer;
 mod object;
 
 pub use self::{
-    database::*, history::*, interface::*, intra_block_state::*, memory_buffer::*, object::*,
+    database::*, history::*, in_memory_state::*, interface::*, intra_block_state::*, object::*,
 };
