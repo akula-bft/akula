321f1df09e19a078273dfd5d7b512c70d0ea0933
Artem Vorotnikov <artem@vorotnikov.me>
2021-09-14 19:41:51 +0300

    Remove get_unwind/set_unwind

--- src/stagedsync/stages.rs
+++ src/stagedsync/stages.rs
@@ -42,12 +42,11 @@ impl Display for StageId {
 
 impl StageId {
     #[instrument]
-    async fn get<'db, Tx: Transaction<'db>, T: Table>(
+    pub async fn get_progress<'db, Tx: Transaction<'db>>(
         &self,
         tx: &Tx,
-        table: &T,
     ) -> anyhow::Result<Option<u64>> {
-        if let Some(b) = tx.get(table, self.as_ref()).await? {
+        if let Some(b) = tx.get(&tables::SyncStage, self.as_ref()).await? {
             return Ok(Some(u64::from_be_bytes(*array_ref![
                 b.get(0..BLOCK_NUMBER_LENGTH)
                     .context("failed to read block number from bytes")?,
@@ -60,46 +59,12 @@ impl StageId {
     }
 
     #[instrument]
-    async fn save<'db, RwTx: MutableTransaction<'db>, T: Table>(
-        &self,
-        tx: &RwTx,
-        table: &T,
-        block: u64,
-    ) -> anyhow::Result<()> {
-        tx.set(table, self.as_ref(), &block.to_be_bytes()).await
-    }
-
-    #[instrument]
-    pub async fn get_progress<'db, Tx: Transaction<'db>>(
-        &self,
-        tx: &Tx,
-    ) -> anyhow::Result<Option<u64>> {
-        self.get(tx, &tables::SyncStage).await
-    }
-
-    #[instrument]
     pub async fn save_progress<'db, RwTx: MutableTransaction<'db>>(
         &self,
         tx: &RwTx,
         block: u64,
     ) -> anyhow::Result<()> {
-        self.save(tx, &tables::SyncStage, block).await
-    }
-
-    #[instrument]
-    pub async fn get_unwind<'db, Tx: Transaction<'db>>(
-        &self,
-        tx: &Tx,
-    ) -> anyhow::Result<Option<u64>> {
-        self.get(tx, &tables::SyncStageUnwind).await
-    }
-
-    #[instrument]
-    pub async fn save_unwind<'db, RwTx: MutableTransaction<'db>>(
-        &self,
-        tx: &RwTx,
-        block: u64,
-    ) -> anyhow::Result<()> {
-        self.save(tx, &tables::SyncStageUnwind, block).await
+        tx.set(&tables::SyncStage, self.as_ref(), &block.to_be_bytes())
+            .await
     }
 }
