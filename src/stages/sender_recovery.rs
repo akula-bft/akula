6a5661de0168392861431158ef7e2420d38e23ee
Jochen Müller <mail@jnmr.de>
2021-10-05 10:59:53 +0200

    Use the ETL in sender recovery stage (#34)

--- src/stages/sender_recovery.rs
+++ src/stages/sender_recovery.rs
@@ -1,5 +1,7 @@
 use crate::{
     accessors::chain,
+    etl::collector::{Collector, OPTIMAL_BUFFER_CAPACITY},
+    kv::tables,
     models::*,
     stagedsync::stage::{ExecOutput, Stage, StageInput, UnwindInput},
     MutableTransaction, StageId,
@@ -22,6 +24,7 @@ const BUFFER_SIZE: u64 = 5000;
 
 async fn process_block<'db: 'tx, 'tx, RwTx>(
     tx: &'tx mut RwTx,
+    collector: &mut Collector,
     height: BlockNumber,
 ) -> anyhow::Result<()>
 where
@@ -40,7 +43,9 @@ where
         senders.push(tx.recover_sender()?);
     }
 
-    chain::tx_sender::write(tx, body.base_tx_id, &senders).await
+    chain::tx_sender::write_to_etl(collector, body.base_tx_id, &senders);
+
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -70,17 +75,23 @@ where
             .unwrap_or_default();
         let to_height = cmp::min(max_height, from_height + BUFFER_SIZE);
 
-        let mut height = from_height;
-        while height < to_height {
-            process_block(tx, height + 1)
-                .await
-                .with_context(|| format!("Failed to recover senders for block {}", height + 1))?;
-            height.0 += 1;
+        let made_progress = to_height > from_height;
+
+        if made_progress {
+            let mut collector = Collector::new(OPTIMAL_BUFFER_CAPACITY);
+
+            for height in from_height + 1..=to_height {
+                process_block(tx, &mut collector, height)
+                    .await
+                    .with_context(|| format!("Failed to recover senders for block {}", height))?;
+            }
+
+            let mut write_cursor = tx.mutable_cursor(&tables::TxSender.erased()).await?;
+            collector.load(&mut write_cursor, None).await?;
         }
 
-        let made_progress = height > from_height;
         Ok(ExecOutput::Progress {
-            stage_progress: height,
+            stage_progress: to_height,
             done: !made_progress,
             must_commit: made_progress,
         })
