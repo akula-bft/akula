6cffd70b9c4487cd21e2110ae38c024eb705df41
Artem Vorotnikov <artem@vorotnikov.me>
2021-12-14 19:43:46 +0300

    Fix block hashes stage

--- src/stages/block_hashes.rs
+++ src/stages/block_hashes.rs
@@ -36,14 +36,14 @@ where
         let original_highest_block = input.stage_progress.unwrap_or(BlockNumber(0));
         let mut highest_block = original_highest_block;
 
-        let mut bodies_cursor = tx.mutable_cursor(&tables::BlockBody).await?;
+        let mut bodies_cursor = tx.mutable_cursor(&tables::CanonicalHeader).await?;
         let mut blockhashes_cursor = tx.mutable_cursor(&tables::HeaderNumber.erased()).await?;
 
         let mut collector = Collector::new(OPTIMAL_BUFFER_CAPACITY);
         let walker = bodies_cursor.walk(Some(highest_block + 1));
         pin!(walker);
 
-        while let Some(((block_number, block_hash), _)) = walker.try_next().await? {
+        while let Some((block_number, block_hash)) = walker.try_next().await? {
             if block_number.0 % 50_000 == 0 {
                 info!("Processing block {}", block_number);
             }
