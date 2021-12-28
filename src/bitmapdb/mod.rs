fdd25ae1dc4482e7ce433648106bffd0fd81d74b
Artem Vorotnikov <artem@vorotnikov.me>
2021-12-28 07:51:45 +0300

    Fix bitmap chunking

--- src/bitmapdb/mod.rs
+++ src/bitmapdb/mod.rs
@@ -55,46 +55,6 @@ where
     Ok(out.unwrap_or_default())
 }
 
-fn cut_left(bm: &mut RoaringTreemap, size_limit: usize) -> Option<RoaringTreemap> {
-    if bm.is_empty() {
-        return None;
-    }
-
-    let sz = bm.get_serialized_size_in_bytes();
-    if sz <= size_limit {
-        let v = std::mem::replace(bm, RoaringTreemap::create());
-
-        return Some(v);
-    }
-
-    let mut v = RoaringTreemap::create();
-
-    let mut it = bm.iter().peekable();
-
-    let mut min_n = None;
-    while let Some(n) = it.peek() {
-        v.add(*n);
-        if v.get_serialized_size_in_bytes() > size_limit {
-            v.remove(*n);
-            min_n = Some(*n);
-            break;
-        }
-        it.next();
-    }
-
-    drop(it);
-
-    if let Some(min_n) = min_n {
-        let to_remove = bm.iter().take_while(|&n| n < min_n).collect::<Vec<_>>();
-        for element in to_remove {
-            bm.remove(element);
-        }
-        Some(v)
-    } else {
-        Some(std::mem::replace(bm, RoaringTreemap::create()))
-    }
-}
-
 pub struct Chunks {
     bm: RoaringTreemap,
     size_limit: usize,
@@ -104,7 +64,45 @@ impl Iterator for Chunks {
     type Item = RoaringTreemap;
 
     fn next(&mut self) -> Option<Self::Item> {
-        cut_left(&mut self.bm, self.size_limit)
+        if self.bm.is_empty() {
+            return None;
+        }
+
+        let sz = self.bm.get_serialized_size_in_bytes();
+        if sz <= self.size_limit {
+            return Some(std::mem::replace(&mut self.bm, RoaringTreemap::create()));
+        }
+
+        let mut v = RoaringTreemap::create();
+
+        let mut it = self.bm.iter().peekable();
+
+        let mut min_n = None;
+        while let Some(n) = it.peek() {
+            v.add(*n);
+            if v.get_serialized_size_in_bytes() > self.size_limit {
+                v.remove(*n);
+                min_n = Some(*n);
+                break;
+            }
+            it.next();
+        }
+
+        drop(it);
+
+        if let Some(min_n) = min_n {
+            let to_remove = self
+                .bm
+                .iter()
+                .take_while(|&n| n < min_n)
+                .collect::<Vec<_>>();
+            for element in to_remove {
+                self.bm.remove(element);
+            }
+            Some(v)
+        } else {
+            Some(std::mem::replace(&mut self.bm, RoaringTreemap::create()))
+        }
     }
 }
 
@@ -148,7 +146,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn cut_left() {
+    fn chunks() {
         for &n in &[1024, 2048] {
             let mut bm = RoaringTreemap::create();
 
@@ -158,10 +156,10 @@ mod tests {
                 }
             }
 
-            while !bm.is_empty() {
-                let lft = super::cut_left(&mut bm, n).unwrap();
+            let mut iter = Chunks::new(bm, n).peekable();
+            while let Some(lft) = iter.next() {
                 let lft_size = lft.get_serialized_size_in_bytes();
-                if !bm.is_empty() {
+                if iter.peek().is_some() {
                     assert!(lft_size > n - 256 && lft_size < n + 256);
                 } else {
                     assert!(lft.get_serialized_size_in_bytes() > 0);
@@ -174,16 +172,15 @@ mod tests {
         {
             let mut bm = RoaringTreemap::create();
             bm.add(1);
-            let lft = super::cut_left(&mut bm, N).unwrap();
-            assert!(lft.get_serialized_size_in_bytes() > 0);
-            assert_eq!(lft.cardinality(), 1);
-            assert_eq!(bm.cardinality(), 0);
+            let mut iter = Chunks::new(bm, N);
+
+            let v = iter.next().unwrap();
+            assert_eq!(v.cardinality(), 1);
+            assert!(v.get_serialized_size_in_bytes() > 0);
+
+            assert_eq!(iter.next(), None);
         }
 
-        {
-            let mut bm = RoaringTreemap::create();
-            assert_eq!(super::cut_left(&mut bm, N), None);
-            assert_eq!(bm.cardinality(), 0);
-        }
+        assert_eq!(Chunks::new(RoaringTreemap::create(), N).next(), None);
     }
 }
