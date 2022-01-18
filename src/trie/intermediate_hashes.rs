a7224c9fa44466251d99797c7cbafc4d8c341ff0
Jochen Müller <mail@jnmr.de>
2022-01-18 20:53:39 +0100

    Node and SubNode implementations

--- src/trie/intermediate_hashes.rs
+++ src/trie/intermediate_hashes.rs
@@ -12,15 +12,50 @@ struct CursorSubNode {
 }
 
 impl CursorSubNode {
-    fn full_key(&self) -> Vec<u8> { todo!() }
+    fn full_key(&self) -> Vec<u8> {
+        let mut out = self.key.clone();
+        if self.nibble >= 0 {
+            out.push(self.nibble as u8)
+        }
+        out
+    }
 
-    fn state_flag(&self) -> bool { todo!() }
+    fn state_flag(&self) -> bool {
+        if self.nibble < 0 || self.node.is_none() {
+            return true;
+        }
+        self.node.as_ref().unwrap().state_mask() & (1u16 << self.nibble) != 0
+    }
 
-    fn tree_flag(&self) -> bool { todo!() }
+    fn tree_flag(&self) -> bool {
+        if self.nibble < 0 || self.node.is_none() {
+            return true;
+        }
+        self.node.as_ref().unwrap().tree_mask() & (1u16 << self.nibble) != 0
+    }
 
-    fn hash_flag(&self) -> bool { todo!() }
+    fn hash_flag(&self) -> bool {
+        if self.node.is_none() {
+            return false;
+        } else if self.nibble < 0 {
+            return self.node.as_ref().unwrap().root_hash().is_some();
+        }
+        self.node.as_ref().unwrap().hash_mask() & (1u16 << self.nibble) != 0
+    }
 
-    fn hash(&self) -> Option<H256> { todo!() }
+    fn hash(&self) -> Option<H256> {
+        if !self.hash_flag() {
+            return None;
+        }
+
+        if self.nibble < 0 {
+            return self.node.as_ref().unwrap().root_hash();
+        }
+
+        let first_nibbles_mask = 1u16 << self.nibble - 1;
+        let hash_idx = (self.node.as_ref().unwrap().hash_mask() & first_nibbles_mask).count_ones();
+        Some(self.node.as_ref().unwrap().hashes()[hash_idx as usize])
+    }
 }
 
 struct Cursor<'cu, 'tx, 'ps, C, T>
@@ -44,15 +79,42 @@ impl<'cu, 'tx, 'ps, C, T> Cursor<'cu, 'tx, 'ps, C, T>
         C: MdbxCursor <'tx, T>
 {
     fn new(cursor: &'cu C, changed: &'ps PrefixSet, prefix: &[u8]) -> Self {
-        todo!();
+        let mut new_cursor = Self {
+            cursor,
+            changed,
+            prefix: prefix.to_vec(),
+            stack: vec![],
+            can_skip_state: false,
+            _marker: PhantomData,
+        };
+        new_cursor.consume_node(&[], true);
+        new_cursor
     }
 
     fn next(&mut self) {
-        todo!();
+        if self.stack.is_empty() {
+            return; // end-of-tree
+        }
+
+        if !self.can_skip_state && self.children_are_in_trie() {
+            if self.stack.last().unwrap().nibble < 0 {
+                self.move_to_next_sibling(true);
+            } else {
+                self.consume_node(&self.key().unwrap(),false);
+            }
+        } else {
+            self.move_to_next_sibling(false);
+        }
+
+        self.update_skip_state();
     }
 
     fn key(&self) -> Option<Vec<u8>> {
-        todo!();
+        if self.stack.is_empty() {
+            None
+        } else {
+            Some(self.stack.last().unwrap().full_key())
+        }
     }
 
     fn hash(&self) -> H256 {
--- src/trie/node.rs
+++ src/trie/node.rs
@@ -1,1 +1,59 @@
-pub(crate) struct Node;
+use ethereum_types::H256;
+
+fn assert_subset(sub: u16, sup: u16) {
+    assert_eq!(sub & sup, sub);
+}
+
+#[derive(Clone, PartialEq)]
+pub(crate) struct Node {
+    state_mask: u16,
+    tree_mask: u16,
+    hash_mask: u16,
+    hashes: Vec<H256>,
+    root_hash: Option<H256>,
+}
+
+impl Node {
+    fn new(
+        state_mask: u16,
+        tree_mask: u16,
+        hash_mask: u16,
+        hashes: Vec<H256>,
+        root_hash: Option<H256>,
+    ) -> Self {
+        assert_subset(tree_mask, state_mask);
+        assert_subset(hash_mask, state_mask);
+        assert_eq!(hash_mask.count_ones() as usize, hashes.len());
+        Self { state_mask, tree_mask, hash_mask, hashes, root_hash }
+    }
+
+    fn assign(&mut self, other: &Node) {
+        self.state_mask = other.state_mask;
+        self.tree_mask = other.tree_mask;
+        self.hash_mask = other.hash_mask;
+        self.hashes = other.hashes.clone();
+        self.root_hash = other.root_hash;
+    }
+
+    pub(crate) fn state_mask(&self) -> u16 { self.state_mask }
+
+    pub(crate) fn tree_mask(&self) -> u16 { self.tree_mask }
+
+    pub(crate) fn hash_mask(&self) -> u16 { self.hash_mask }
+
+    pub(crate) fn hashes(&self) -> Vec<H256> { self.hashes.clone() }
+
+    pub(crate) fn root_hash(&self) -> Option<H256> { self.root_hash }
+
+    fn set_root_hash(&mut self, root_hash: Option<H256>) {
+        self.root_hash = root_hash;
+    }
+}
+
+fn marshal_node(node: &Node) -> Vec<u8> {
+    todo!();
+}
+
+fn unmarshal_node(v: &[u8]) -> Option<Node> {
+    todo!();
+}
