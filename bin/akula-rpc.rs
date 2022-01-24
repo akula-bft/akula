22769e31d26433d3551ab6e623dab45a0dc93961
Emilia Hane <emiliaha95@gmail.com>
2022-01-24 01:39:29 +0100

    Continue impl EthApi trait from rust-ethereum for rpc server

--- bin/akula-rpc.rs
+++ bin/akula-rpc.rs
@@ -1,13 +1,18 @@
 use akula::{
-    accessors::{chain::*, state::account},
+    accessors::{
+        chain::*,
+        state::{account, storage},
+    },
     binutil::AkulaDataDir,
     kv::{tables, traits::*},
     models::*,
     stagedsync::stages::*,
 };
 use async_trait::async_trait;
+use bytes::Bytes;
 use clap::Parser;
 use ethereum_types::{Address, U256, U64, *};
+use jsonrpc::common::{StoragePos, TxIndex, UncleIndex};
 use jsonrpsee::{core::RpcResult, http_server::HttpServerBuilder, proc_macros::rpc};
 use std::{future::pending, net::SocketAddr, sync::Arc};
 use tracing_subscriber::{prelude::*, EnvFilter};
@@ -34,14 +39,53 @@ pub trait EthApi {
         block_hash: H256,
         full_tx_obj: bool,
     ) -> RpcResult<jsonrpc::common::Block>;
-    /*#[method(name = "getBlockByNumber")]
+    #[method(name = "getBlockByNumber")]
     async fn get_block_by_number(
         &self,
         block_number: BlockNumber,
         full_tx_obj: bool,
-    ) -> RpcResult<Block>;*/
+    ) -> RpcResult<jsonrpc::common::Block>;
     #[method(name = "getBlockTransactionCountByHash")]
     async fn get_block_tx_count_by_hash(&self, block_hash: H256) -> RpcResult<U64>;
+    #[method(name = "getBlockTransactionCountByNumber")]
+    async fn get_block_tx_count_by_number(&self, block_number: BlockNumber) -> RpcResult<U64>;
+    #[method(name = "getCode")]
+    async fn get_code(&self, address: Address, block_number: BlockNumber) -> RpcResult<Bytes>;
+    #[method(name = "getStorageAt")]
+    async fn get_storage_at(
+        &self,
+        address: Address,
+        storage_pos: StoragePos,
+        block_number: BlockNumber,
+    ) -> RpcResult<U256>;
+    #[method(name = "getTransactionByBlockHashAndIndex")]
+    async fn get_tx_by_block_hash_and_index(
+        &self,
+        block_hash: H256,
+        index: TxIndex,
+    ) -> RpcResult<jsonrpc::common::Tx>;
+    #[method(name = "getTransactionByBlockNumberAndIndex")]
+    async fn get_tx_by_block_number_and_index(
+        &self,
+        block_number: BlockNumber,
+        index: TxIndex,
+    ) -> RpcResult<jsonrpc::common::Tx>;
+    #[method(name = "getUncleByBlockHashAndIndex")]
+    async fn get_uncle_by_block_hash_and_index(
+        &self,
+        block_hash: H256,
+        index: UncleIndex,
+    ) -> RpcResult<jsonrpc::common::Block>;
+    #[method(name = "getUncleByBlockNumberAndIndex")]
+    async fn get_uncle_by_block_number_and_index(
+        &self,
+        block_number: BlockNumber,
+        index: UncleIndex,
+    ) -> RpcResult<jsonrpc::common::Block>;
+    #[method(name = "getUncleCountByBlockHash")]
+    async fn get_uncle_count_by_block_hash(&self, block_hash: H256) -> RpcResult<U64>;
+    #[method(name = "getUncleCountByBlockNumber")]
+    async fn get_uncle_count_by_block_number(&self, block_number: BlockNumber) -> RpcResult<U64>;
 }
 
 pub struct EthApiServerImpl<DB>
@@ -87,7 +131,7 @@ where
                 .unwrap();
 
         let msgs = block_body.transactions;
-        let index = 0;
+        let mut index = 0;
         let mut msg_hashes: Vec<jsonrpc::common::Transaction> = Vec::new();
         while index < msgs.len() {
             match full_tx_obj {
@@ -95,9 +139,10 @@ where
                 true => {
                     let tx =
                         json_tx::assemble_tx(block_hash, block_number, &msgs[index], index).await;
-                    msg_hashes.push(tx)
+                    msg_hashes.push(jsonrpc::common::Transaction::Full(Box::new(tx)));
                 }
             }
+            index = index + 1;
         }
 
         let total_difficulty = td::read(&self.db.begin().await?, block_hash, block_number)
@@ -144,13 +189,78 @@ where
         })
     }
 
-    /*async fn get_block_by_number(
+    async fn get_block_by_number(
         &self,
         block_number: BlockNumber,
         full_tx_obj: bool,
-    ) -> RpcResult<Block> {
+    ) -> RpcResult<jsonrpc::common::Block> {
+        let block_hash = canonical_hash::read(&self.db.begin().await?, block_number)
+            .await?
+            .unwrap();
 
-    }*/
+        let block_body =
+            block_body::read_without_senders(&self.db.begin().await?, block_hash, block_number)
+                .await?
+                .unwrap();
+
+        let msgs = block_body.transactions;
+        let mut index = 0;
+        let mut msg_hashes: Vec<jsonrpc::common::Transaction> = Vec::new();
+        while index < msgs.len() {
+            match full_tx_obj {
+                false => msg_hashes.push(jsonrpc::common::Transaction::Partial(msgs[index].hash())),
+                true => {
+                    let tx =
+                        json_tx::assemble_tx(block_hash, block_number, &msgs[index], index).await;
+                    msg_hashes.push(jsonrpc::common::Transaction::Full(Box::new(tx)));
+                }
+            }
+            index = index + 1;
+        }
+
+        let total_difficulty = td::read(&self.db.begin().await?, block_hash, block_number)
+            .await?
+            .unwrap();
+
+        let size = rlp::encode(
+            &(block_body::read_without_senders(&self.db.begin().await?, block_hash, block_number)
+                .await?
+                .unwrap()),
+        )
+        .len();
+
+        let ommers = block_body.ommers;
+        let mut ommer_hashes: Vec<H256> = Vec::new();
+        for ommer in ommers.iter() {
+            ommer_hashes.push(ommer.hash());
+        }
+
+        let header = header::read(&self.db.begin().await?, block_hash, block_number)
+            .await?
+            .unwrap();
+
+        Ok(jsonrpc::common::Block {
+            number: Some(U64::from(block_number.0)),
+            hash: Some(block_hash),
+            parent_hash: header.parent_hash,
+            nonce: Some(header.nonce),
+            sha3_uncles: header.ommers_hash,
+            logs_bloom: Some(header.logs_bloom),
+            transactions_root: header.transactions_root,
+            state_root: header.state_root,
+            receipts_root: header.receipts_root,
+            miner: header.beneficiary,
+            difficulty: header.difficulty,
+            total_difficulty,
+            extra_data: header.extra_data,
+            size: U64::from(size),
+            gas_limit: U64::from(header.gas_limit),
+            gas_used: U64::from(header.gas_used),
+            timestamp: U64::from(header.timestamp),
+            transactions: msg_hashes,
+            uncles: ommer_hashes,
+        })
+    }
 
     async fn get_block_tx_count_by_hash(&self, block_hash: H256) -> RpcResult<U64> {
         let block_number = header_number::read(&self.db.begin().await?, block_hash)
@@ -163,6 +273,272 @@ where
 
         Ok(block_body.tx_amount.into())
     }
+
+    async fn get_block_tx_count_by_number(&self, block_number: BlockNumber) -> RpcResult<U64> {
+        let block_hash = canonical_hash::read(&self.db.begin().await?, block_number)
+            .await?
+            .unwrap();
+
+        let block_body = storage_body::read(&self.db.begin().await?, block_hash, block_number)
+            .await?
+            .unwrap();
+
+        Ok(block_body.tx_amount.into())
+    }
+
+    async fn get_code(&self, address: Address, block_number: BlockNumber) -> RpcResult<Bytes> {
+        let code_hash = account::read(&self.db.begin().await?, address, Some(block_number))
+            .await?
+            .unwrap()
+            .code_hash;
+
+        Ok(
+            akula::read_account_code(&self.db.begin().await?, Address::from([1; 20]), code_hash)
+                .await?
+                .unwrap(),
+        )
+    }
+
+    async fn get_storage_at(
+        &self,
+        address: Address,
+        storage_pos: StoragePos,
+        block_number: BlockNumber,
+    ) -> RpcResult<U256> {
+        Ok(storage::read(
+            &self.db.begin().await?,
+            address,
+            storage_pos,
+            Some(block_number),
+        )
+        .await?)
+    }
+
+    async fn get_tx_by_block_hash_and_index(
+        &self,
+        block_hash: H256,
+        index: TxIndex,
+    ) -> RpcResult<jsonrpc::common::Tx> {
+        let block_number = header_number::read(&self.db.begin().await?, block_hash)
+            .await?
+            .unwrap();
+
+        let block_body =
+            block_body::read_without_senders(&self.db.begin().await?, block_hash, block_number)
+                .await?
+                .unwrap();
+
+        let i = index.as_u64() as usize;
+        let msgs = block_body.transactions;
+        Ok(json_tx::assemble_tx(block_hash, block_number, &msgs[i], i).await)
+    }
+
+    async fn get_tx_by_block_number_and_index(
+        &self,
+        block_number: BlockNumber,
+        index: TxIndex,
+    ) -> RpcResult<jsonrpc::common::Tx> {
+        let block_hash = canonical_hash::read(&self.db.begin().await?, block_number)
+            .await?
+            .unwrap();
+
+        let block_body =
+            block_body::read_without_senders(&self.db.begin().await?, block_hash, block_number)
+                .await?
+                .unwrap();
+
+        let i = index.as_u64() as usize;
+        let msgs = block_body.transactions;
+        Ok(json_tx::assemble_tx(block_hash, block_number, &msgs[i], i).await)
+    }
+
+    async fn get_uncle_by_block_hash_and_index(
+        &self,
+        block_hash: H256,
+        index: UncleIndex,
+    ) -> RpcResult<jsonrpc::common::Block> {
+        let block_number = header_number::read(&self.db.begin().await?, block_hash)
+            .await?
+            .unwrap();
+
+        let block_body =
+            block_body::read_without_senders(&self.db.begin().await?, block_hash, block_number)
+                .await?
+                .unwrap();
+
+        let ommer_hash = block_body.ommers[index.as_u64() as usize].hash();
+
+        let ommer_block_number = header_number::read(&self.db.begin().await?, ommer_hash)
+            .await?
+            .unwrap();
+
+        let ommer = block_body::read_without_senders(
+            &self.db.begin().await?,
+            ommer_hash,
+            ommer_block_number,
+        )
+        .await?
+        .unwrap();
+
+        let msgs = ommer.transactions;
+        let mut index = 0;
+        let mut msg_hashes: Vec<jsonrpc::common::Transaction> = Vec::new();
+        while index < msgs.len() {
+            let tx = json_tx::assemble_tx(block_hash, block_number, &msgs[index], index).await;
+            msg_hashes.push(jsonrpc::common::Transaction::Full(Box::new(tx)));
+            index = index + 1;
+        }
+
+        let total_difficulty = td::read(&self.db.begin().await?, block_hash, block_number)
+            .await?
+            .unwrap();
+
+        let size = rlp::encode(
+            &(block_body::read_without_senders(&self.db.begin().await?, block_hash, block_number)
+                .await?
+                .unwrap()),
+        )
+        .len();
+
+        let ommers = ommer.ommers;
+        let mut ommer_hashes: Vec<H256> = Vec::new();
+        for ommer in ommers.iter() {
+            ommer_hashes.push(ommer.hash());
+        }
+
+        let header = header::read(&self.db.begin().await?, block_hash, block_number)
+            .await?
+            .unwrap();
+
+        Ok(jsonrpc::common::Block {
+            number: Some(U64::from(block_number.0)),
+            hash: Some(block_hash),
+            parent_hash: header.parent_hash,
+            nonce: Some(header.nonce),
+            sha3_uncles: header.ommers_hash,
+            logs_bloom: Some(header.logs_bloom),
+            transactions_root: header.transactions_root,
+            state_root: header.state_root,
+            receipts_root: header.receipts_root,
+            miner: header.beneficiary,
+            difficulty: header.difficulty,
+            total_difficulty,
+            extra_data: header.extra_data,
+            size: U64::from(size),
+            gas_limit: U64::from(header.gas_limit),
+            gas_used: U64::from(header.gas_used),
+            timestamp: U64::from(header.timestamp),
+            transactions: msg_hashes,
+            uncles: ommer_hashes,
+        })
+    }
+
+    async fn get_uncle_by_block_number_and_index(
+        &self,
+        block_number: BlockNumber,
+        index: UncleIndex,
+    ) -> RpcResult<jsonrpc::common::Block> {
+        let block_hash = canonical_hash::read(&self.db.begin().await?, block_number)
+            .await?
+            .unwrap();
+
+        let block_body =
+            block_body::read_without_senders(&self.db.begin().await?, block_hash, block_number)
+                .await?
+                .unwrap();
+
+        let ommer_hash = block_body.ommers[index.as_u64() as usize].hash();
+
+        let ommer_block_number = header_number::read(&self.db.begin().await?, ommer_hash)
+            .await?
+            .unwrap();
+
+        let ommer = block_body::read_without_senders(
+            &self.db.begin().await?,
+            ommer_hash,
+            ommer_block_number,
+        )
+        .await?
+        .unwrap();
+
+        let msgs = ommer.transactions;
+        let mut index = 0;
+        let mut msg_hashes: Vec<jsonrpc::common::Transaction> = Vec::new();
+        while index < msgs.len() {
+            let tx = json_tx::assemble_tx(block_hash, block_number, &msgs[index], index).await;
+            msg_hashes.push(jsonrpc::common::Transaction::Full(Box::new(tx)));
+            index = index + 1;
+        }
+
+        let total_difficulty = td::read(&self.db.begin().await?, block_hash, block_number)
+            .await?
+            .unwrap();
+
+        let size = rlp::encode(
+            &(block_body::read_without_senders(&self.db.begin().await?, block_hash, block_number)
+                .await?
+                .unwrap()),
+        )
+        .len();
+
+        let ommers = ommer.ommers;
+        let mut ommer_hashes: Vec<H256> = Vec::new();
+        for ommer in ommers.iter() {
+            ommer_hashes.push(ommer.hash());
+        }
+
+        let header = header::read(&self.db.begin().await?, block_hash, block_number)
+            .await?
+            .unwrap();
+
+        Ok(jsonrpc::common::Block {
+            number: Some(U64::from(block_number.0)),
+            hash: Some(block_hash),
+            parent_hash: header.parent_hash,
+            nonce: Some(header.nonce),
+            sha3_uncles: header.ommers_hash,
+            logs_bloom: Some(header.logs_bloom),
+            transactions_root: header.transactions_root,
+            state_root: header.state_root,
+            receipts_root: header.receipts_root,
+            miner: header.beneficiary,
+            difficulty: header.difficulty,
+            total_difficulty,
+            extra_data: header.extra_data,
+            size: U64::from(size),
+            gas_limit: U64::from(header.gas_limit),
+            gas_used: U64::from(header.gas_used),
+            timestamp: U64::from(header.timestamp),
+            transactions: msg_hashes,
+            uncles: ommer_hashes,
+        })
+    }
+
+    async fn get_uncle_count_by_block_hash(&self, block_hash: H256) -> RpcResult<U64> {
+        let block_number = header_number::read(&self.db.begin().await?, block_hash)
+            .await?
+            .unwrap();
+
+        let block_body =
+            block_body::read_without_senders(&self.db.begin().await?, block_hash, block_number)
+                .await?
+                .unwrap();
+
+        Ok(block_body.ommers.len().into())
+    }
+
+    async fn get_uncle_count_by_block_number(&self, block_number: BlockNumber) -> RpcResult<U64> {
+        let block_hash = canonical_hash::read(&self.db.begin().await?, block_number)
+            .await?
+            .unwrap();
+
+        let block_body =
+            block_body::read_without_senders(&self.db.begin().await?, block_hash, block_number)
+                .await?
+                .unwrap();
+
+        Ok(block_body.ommers.len().into())
+    }
 }
 
 pub mod json_tx {
@@ -173,7 +549,7 @@ pub mod json_tx {
         block_number: BlockNumber,
         msg: &MessageWithSignature,
         index: usize,
-    ) -> jsonrpc::common::Transaction {
+    ) -> jsonrpc::common::Tx {
         let v = YParityAndChainId {
             odd_y_parity: msg.signature.odd_y_parity(),
             chain_id: msg.chain_id(),
@@ -184,7 +560,7 @@ pub mod json_tx {
             TransactionAction::Create => None,
         };
 
-        jsonrpc::common::Transaction::Full(Box::new(jsonrpc::common::Tx {
+        jsonrpc::common::Tx {
             block_hash: Some(block_hash),
             block_number: Some(U64::from(block_number.0)),
             from: msg.recover_sender().unwrap(),
@@ -199,7 +575,7 @@ pub mod json_tx {
             v: U64::from(v.v()),
             r: msg.r(),
             s: msg.s(),
-        }))
+        }
     }
 }
 
