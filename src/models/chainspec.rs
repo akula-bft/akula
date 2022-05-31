e4d657987ec05dcc84c2d2519298889fc266c363
Eugene Gelfand <jewgeni@protonmail.com>
2022-05-31 15:35:06 +0200

    fix test

--- src/models/chainspec.rs
+++ src/models/chainspec.rs
@@ -489,7 +489,7 @@ mod tests {
                         "enode://343149e4feefa15d882d9fe4ac7d88f885bd05ebb735e547f12e12080a9fa07c8014ca6fd7f373123488102fe5e34111f8509cf0b7de3f5b44339c9f25e87cb8@52.3.158.184:30303",
                         "enode://b6b28890b006743680c52e64e0d16db57f28124885595fa03a562be1d2bf0f3a1da297d56b13da25fb992888fd556d4c1a27b1f39d531bde7de1921c90061cc6@159.89.28.211:30303",
                     ].into_iter().map(ToString::to_string).collect(),
-                    dns: Some("all.goerli.ethdisco.net".to_owned()),
+                    dns: Some("all.rinkeby.ethdisco.net".to_owned()),
                 }
             },
             *RINKEBY,
