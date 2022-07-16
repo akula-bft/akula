435868f2fb20699176b4b3d1e569772e48960d17
Artem Vorotnikov <artem@vorotnikov.me>
2022-07-17 02:08:03 +0300

    Fix again

--- src/rpc/net.rs
+++ src/rpc/net.rs
@@ -1,6 +1,6 @@
 use crate::models::*;
 use async_trait::async_trait;
-use ethereum_jsonrpc::NetApiServer;
+use ethereum_jsonrpc::{types::StringU64, NetApiServer};
 use jsonrpsee::core::RpcResult;
 
 pub struct NetApiServerImpl {
@@ -15,7 +15,7 @@ impl NetApiServer for NetApiServerImpl {
     async fn peer_count(&self) -> RpcResult<U64> {
         Ok(U64::zero())
     }
-    async fn version(&self) -> RpcResult<U64> {
+    async fn version(&self) -> RpcResult<StringU64> {
         Ok(self.network_id.0.into())
     }
 }
