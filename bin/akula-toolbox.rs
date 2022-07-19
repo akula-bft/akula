46759cccc11ab1cd75c0ccfecb08f8373622fb08
Artem Vorotnikov <artem@vorotnikov.me>
2022-07-19 13:22:51 +0300

    Fix overwite-chainspec

--- bin/akula-toolbox.rs
+++ bin/akula-toolbox.rs
@@ -495,6 +495,7 @@ fn overwrite_chainspec(data_dir: AkulaDataDir, chainspec_file: PathBuf) -> anyho
     let tx = env.begin_mutable()?;
 
     tx.set(tables::Config, (), new_chainspec)?;
+    tx.commit()?;
 
     Ok(())
 }
